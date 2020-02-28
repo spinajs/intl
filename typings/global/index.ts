85a08a3c5f4b01dbe4ba7ef28215f5dec09f8858
Grzegorz Beń <grzegorz.ben@coderush.pl>
2020-02-29 00:48:20 +0100

    initial intl implementation

---
+++ typings/global/index.ts
@@ -0,0 +1,79 @@
+declare global {
+	declare namespace NodeJS {
+		interface Global {
+		  /**
+		   * I18n localization function. Returns localized string.
+		   * If no translation is avaible at current selected language, then fallback to
+		   * default language, if still no translation exists, original text is returned
+		   *
+		   * @param text { string } - text to localize.
+		   * @param locale { string } - selected locale, if not specified - default locale is selected
+		   */
+		  __(text: string, locale?: string): string;
+	  
+		  /**
+		   * Plurals translation of a single phrase. Singular and plural forms will get added to locales if unknown.
+		   * Returns translated parsed and substituted string based on last count parameter.
+		   *
+		   * @param text { string } - text to localize
+		   * @param count { number } - number of items/things
+		   * @example use like `__n("%s cats", 1) returns `1 cat`
+		   */
+		  __n(text: string, count: Number): string;
+	  
+		  /**
+		   * Returns a list of translations for a given phrase in each language.
+		   *
+		   * @param text { string } - text to translate
+		   */
+		  __l(text: string): string[];
+	  
+		  /**
+		   * Returns a hashed list of translations for a given phrase in each language.
+		   *
+		   * @param text { string } - text to translate
+		   */
+		  __h(text: string): any[];
+		}
+	  }
+	  
+	  interface PhraseWithOptions {
+		phrase: string;
+		locale: string;
+	  }
+	  
+	  /**
+	   * I18n localization function. Returns localized string.
+	   * If no translation is avaible at current selected language, then fallback to
+	   * default language, if still no translation exists, original text is returned
+	   *
+	   * @param text { string } - text to localize.
+	   * @param locale { string } - selected locale, if not specified - default locale is selected
+	   */
+	  declare function __(text: string | PhraseWithOptions, ...args: any[]): string;
+	  
+	  /**
+	   * Plurals translation of a single phrase. Singular and plural forms will get added to locales if unknown.
+	   * Returns translated parsed and substituted string based on last count parameter.
+	   *
+	   * @param text { string } - text to localize
+	   * @param count { number } - number of items/things
+	   * @example use like `__n("%s cats", 1) returns `1 cat`
+	   */
+	  declare function __n(text: string, count: Number): string;
+	  
+	  /**
+	   * Returns a list of translations for a given phrase in each language.
+	   *
+	   * @param text { string } - text to translate
+	   */
+	  declare function __l(text: string): string[];
+	  
+	  /**
+	   * Returns a hashed list of translations for a given phrase in each language.
+	   *
+	   * @param text { string } - text to translate
+	   */
+	  declare function __h(text: string): any[];
+}
+  

36b04b1916910f835b0830bcfda98d440715fa4c
Grzegorz Beń <grzegorz.ben@coderush.pl>
2020-06-18 00:42:19 +0200

    ver updates

--- src/config/locales.ts
+++ src/config/locales.ts
@@ -3,12 +3,12 @@
  * https://github.com/mashpie/i18n-node
  */
 module.exports.intl = {
-    // supported locales
-    locales: ['en', 'pl'],
-  
-    defaultLocale: 'pl',
-  
-    // query parameter to switch locale (ie. /home?lang=ch) - defaults to NULL
-    queryParameter: 'lang',
-  };
-  
+
+  defaultLocale: 'pl',
+
+  // supported locales
+  locales: ['en', 'pl'],
+
+  // query parameter to switch locale (ie. /home?lang=ch) - defaults to NULL
+  queryParameter: 'lang',
+};
